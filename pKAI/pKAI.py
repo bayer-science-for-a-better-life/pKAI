177366ab3d60d48e9cbe5e772d1e71a6c413a31a
pedrishi <pdreis@fc.ul.pt>
2020-12-03 17:56:38 +0100

    fixed entry points for CLI usage

--- pKAI/pKAI.py
+++ pKAI/pKAI.py
@@ -24,7 +24,7 @@ def download_model(file_name: str, file_path: str) -> None:
         )
 
 
-def load_model(model_name: str):
+def load_model(model_name: str, device):
     path = dirname(abspath(__file__))
     fname = f"{model_name}_model.pt"
     fpath = f"{path}/{fname}"
@@ -32,7 +32,7 @@ def load_model(model_name: str):
     if not isfile(fpath):
         download_model(fname, fpath)
 
-    device = torch.device(args.device)
+    device = torch.device(device)
     model = torch.jit.load(f"{fpath}").to(device)
 
     return model
@@ -41,14 +41,14 @@ def load_model(model_name: str):
 def pKAI(pdb, model_name="pKAI", device="cpu", threads=None):
     if threads:
         torch.set_num_threads(threads)
-    model = load_model(model_name)
+    model = load_model(model_name, device)
     prot = Protein(pdb)
     prot.apply_cutoff()
     pks = prot.predict_pkas(model, device)
     return pks
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description="pKAI")
     parser.add_argument("pdb_file", type=str, help="PDB file")
     parser.add_argument(
@@ -66,4 +66,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     pKAI(args.pdb_file, model_name=args.model, device=args.device, threads=args.threads)
+
+
+if __name__ == "__main__":
+    main()
 
